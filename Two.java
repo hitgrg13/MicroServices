94cd4fa73ab266f6710ddcc9f0f83e78597c27b8
hitkaji <hitgrg13@gmail.com>
2023-08-26 17:27:41 -0500

    extra line added

---
+++ Two.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Two {
+
+    public static void main(String[] args) {
+
+
+   List<Integer> a = new ArrayList<Integer>();
+   a.add(10);
+   a.add(20);
+   a.add(23);
+   for(Integer b:a)
+       System.out.println(b);
+
+       System.out.println(a.toString());
+    }
+}
